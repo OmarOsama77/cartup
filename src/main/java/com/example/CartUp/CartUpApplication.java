1b2d600127133a0557346cead799aa8384010818
Omar Osama <omarosama974123@gmail.com>
2026-07-18 10:57:14 +0300

    first commit

---
+++ src/main/java/com/example/CartUp/CartUpApplication.java
@@ -0,0 +1,13 @@
+package com.example.CartUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartUpApplication.class, args);
+	}
+
+}
