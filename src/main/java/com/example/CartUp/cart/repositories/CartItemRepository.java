7c5db72060244cf5bcd0c321930d94898cdfa29d
Omar Osama <omarosama974123@gmail.com>
2026-08-20 17:48:57 +0300

    updates on carts repo

--- src/main/java/com/example/CartUp/cart/repositories/CartItemRepository.java
+++ src/main/java/com/example/CartUp/cart/repositories/CartItemRepository.java
@@ -1,15 +1,8 @@
 package com.example.CartUp.cart.repositories;
 
 import com.example.CartUp.cart.entities.CartItem;
-import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
-    @Modifying
-    @Transactional
-    @Query(value = "delete from cart_items where cart_id =:cartId and product_variant_id =productVariantId " , nativeQuery = true)
-    int deleteProductInCart(@Param("cartId") Long cartId,@Param("productVariantId") Long productVariantId );
+
 }
--- src/main/java/com/example/CartUp/cart/repositories/CartRepository.java
+++ src/main/java/com/example/CartUp/cart/repositories/CartRepository.java
@@ -7,6 +7,6 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface CartRepository extends JpaRepository<Cart,Long> {
-    boolean existsByUserId(UUID userId);
+
     Optional<Cart> findByUserId(UUID userId);
 }
